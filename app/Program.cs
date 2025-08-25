2342fbd2c82c59b4f310281a0aec132b2b4b221d
LeyLey30 <Wesley30br@gmail.com>
2025-08-25 09:07:46 -0300

    Atualização
    Atualização para atividade

---
+++ app/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Print(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        static double CalcularAreaCirculo(double raio)
+        {
+            double saida = Math.PI * Math.Pow(raio, 2);
+            return saida;
+        }
+
+        static void Main(string[] args)
+        {
+            Print("Hello World!");
+            double area = CalcularAreaCirculo(12);
+            Print(area.ToString());
+        }
+    }
+}
